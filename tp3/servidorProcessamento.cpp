a6d1a5d0f937bb85240d20e76ef3f87be0b81f10
guinasc2 <guiadnguto@gmail.com>
2022-11-01 20:07:29 -0300

    Update servidorProcessamento.cpp

--- tp3/servidorProcessamento.cpp
+++ tp3/servidorProcessamento.cpp
@@ -13,51 +13,44 @@
 using namespace std;
 using namespace sw::redis;
 
-typedef struct
-{
+#define topicoCliente "processamento"
+#define topicoServidores "processamentoInterno"
+#define numServidores 11
+
+typedef struct {
 	int id;
 	string conteudo;
 } Mensagem;
 
-typedef struct
-{
+typedef struct {
 	string conteudo;
 	bool valido;
 } Data;
 
-typedef struct
-{
+typedef struct {
 	int idServidor;
 	int tentativa;
 } Try;
 
-typedef struct
-{
+typedef struct {
 	Try tentativas[11];
 	size_t size;
 } TryQueue;
 
-typedef struct
-{
+typedef struct {
 	int posicao;
 	string conteudo;
 } Escrita;
 
-typedef struct
-{
+typedef struct {
 	bool pronto;
 	string conteudo;
 } Resposta;
 
-#define topicoCliente "processamento"
-#define topicoServidores "processamentoInterno"
-
-class ServidorProcessamento
-{
+class ServidorProcessamento {
 
 public:
-	ConnectionOptions _build_options(string host, int port, string password)
-	{
+	ConnectionOptions _build_options(string host, int port, string password) {
 		ConnectionOptions opts;
 		opts.host = host;
 		opts.port = port;
@@ -76,42 +69,35 @@ public:
 	static bool requestIdPrimario, temPrimario;
 	static Resposta resposta;
 
-	ServidorProcessamento(string host = "127.0.0.1", int port = 6379, string password = "", int idPeer = 0) : connection_options_(_build_options(host, port, password)),
-																											  redis_(connection_options_), sub(redis_.subscriber())
-	{
-		sub.on_message(tratarMensagem);
-		id = idPeer;
-		idPrimario = -1;
-		requestIdPrimario = false;
-		temPrimario = false;
-		resposta.pronto = false;
-		for (int i = 0; i < 100; i++)
-			array[i].valido = false;
-		srand(time(NULL));
+	ServidorProcessamento(string host = "127.0.0.1", int port = 6379, string password = "", int idPeer = 0) : 
+		connection_options_(_build_options(host, port, password)),
+		redis_(connection_options_), sub(redis_.subscriber()) {
+			sub.on_message(tratarMensagem);
+			id = idPeer;
+			idPrimario = -1;
+			requestIdPrimario = false;
+			temPrimario = false;
+			resposta.pronto = false;
+			for (int i = 0; i < 100; i++)
+				array[i].valido = false;
+			srand(time(NULL));
 	}
 
-	void subscribe(string canal)
-	{
+	void subscribe(string canal) {
 		sub.subscribe(canal);
 	}
 
-	void consumeLoop()
-	{
-		while (true)
-		{
-			try
-			{
+	void consumeLoop() {
+		while (true) {
+			try {
 				sub.consume();
-			}
-			catch (const Error &err)
-			{
+			} catch (const Error &err) {
 				cout << err.what() << endl;
 			}
 		}
 	}
 
-	void startPub()
-	{
+	void startPub() {
 		int tentativa = rand() % 100;
 
 		string message = "temPrimario?" + to_string(tentativa);
@@ -121,8 +107,7 @@ public:
 		if (!temPrimario)
 			idPrimario = ordem.tentativas[0].idServidor;
 		cout << "DEBUG -> ordem.size = " << ordem.size << endl;
-		for (int i = 0; i < ordem.size; i++)
-		{
+		for (int i = 0; i < ordem.size; i++) {
 			cout << "DEBUG -> " << ordem.tentativas[i].idServidor << ":" << ordem.tentativas[i].tentativa << endl;
 		}
 
@@ -130,13 +115,11 @@ public:
 		cout << "DEBUG -> " << message << endl;
 	}
 
-	void startSub()
-	{
+	void startSub() {
 		consumeLoop();
 	}
 
-	void start()
-	{
+	void start() {
 		cout << "Servidor de armazenamento " << id << " iniciado." << endl;
 
 		thread subscriber(&ServidorProcessamento::startSub, this);
@@ -149,26 +132,21 @@ public:
 		publisher.join();
 	}
 
-	string makeMessage(string mensagem)
-	{
+	string makeMessage(string mensagem) {
 		string m = to_string(id) + ":" + mensagem;
 		return m;
 	}
 
-	void responderMensagens()
-	{
+	void responderMensagens() {
 		string message;
-		while (true)
-		{
-			if (requestIdPrimario)
-			{
+		while (true) {
+			if (requestIdPrimario) {
 				message = "temPrimario!" + to_string(id);
 				redis_.publish(topicoServidores, makeMessage(message));
 				requestIdPrimario = false;
 			}
 
-			if (resposta.pronto)
-			{
+			if (resposta.pronto) {
 				message = "response:" + resposta.conteudo;
 				redis_.publish(topicoCliente, message);
 				resposta.pronto = false;
@@ -176,14 +154,12 @@ public:
 		}
 	}
 
-	static void insertTry(Try t)
-	{
+	static void insertTry(Try t) {
 		if (ordem.size == 11)
 			return;
 
 		int i;
-		for (i = ordem.size - 1; i >= 0; i--)
-		{
+		for (i = ordem.size - 1; i >= 0; i--) {
 			if (ordem.tentativas[i].tentativa < t.tentativa)
 				ordem.tentativas[i + 1] = ordem.tentativas[i];
 			else
@@ -194,13 +170,11 @@ public:
 		ordem.size++;
 	}
 
-	static void tratarMensagem(string canal, string mensagem)
-	{
+	static void tratarMensagem(string canal, string mensagem) {
 		cout << "DEBUG Nova mensagem -> " << canal << ": " << mensagem << endl;
 		string message;
 
-		if (canal.compare(topicoServidores) == 0)
-		{ // conversa interna entre os servidores
+		if (canal.compare(topicoServidores) == 0) { // conversa interna entre os servidores
 			size_t token = mensagem.find_first_of(":", 0);
 			string idString = mensagem.substr(0, token);
 			string conteudo = mensagem.substr(token + 1, mensagem.size());
@@ -209,14 +183,10 @@ public:
 			msg.id = stoi(idString);
 			msg.conteudo = conteudo;
 
-			if (msg.conteudo.find("temPrimario?", 0) != std::string::npos)
-			{
-				if (id == idPrimario)
-				{ // servidor primário recebeu essa mensagem
+			if (msg.conteudo.find("temPrimario?", 0) != std::string::npos) {
+				if (id == idPrimario) { // servidor primário recebeu essa mensagem
 					requestIdPrimario = true;
-				}
-				else if (idPrimario == -1)
-				{
+				} else if (idPrimario == -1) {
 					Try t;
 					token = msg.conteudo.find_first_of("?", 0);
 					conteudo = msg.conteudo.substr(token + 1, msg.conteudo.size());
@@ -224,23 +194,17 @@ public:
 					t.tentativa = stoi(conteudo);
 					insertTry(t);
 				}
-			}
-			else if (msg.conteudo.find("temPrimario!") != std::string::npos && idPrimario == -1)
-			{
+			} else if (msg.conteudo.find("temPrimario!") != std::string::npos && idPrimario == -1) {
 				token = msg.conteudo.find_first_of("!", 0);
 				conteudo = msg.conteudo.substr(token + 1, msg.conteudo.size());
 				temPrimario = true;
 				idPrimario = stoi(conteudo);
 			}
-		}
-		else if (canal.compare(topicoCliente) == 0)
-		{ // mensagem do cliente
+		} else if (canal.compare(topicoCliente) == 0) { // mensagem do cliente
+			if (id == idPrimario && mensagem.find("response:") == std::string::npos) { // açoes do servidor primário
 
-			if (id == idPrimario && mensagem.find("response:") == std::string::npos)
-			{ // açoes do servidor primário
 				cout << "DEBUG -> Servidor primário recebeu mensagem do cliente" << endl;
-				if (mensagem.find("fib") != std::string::npos)
-				{
+				if (mensagem.find("fib") != std::string::npos) {
 
 					cout << "DEBUG Fibonacci -> " + mensagem << endl;
 					size_t token = mensagem.find_first_of(",", 0);
@@ -249,9 +213,7 @@ public:
 					message = to_string(fibonacci(stoi(valorString)));
 					resposta.conteudo = message;
 					resposta.pronto = true;
-				}
-				else if (mensagem.find("exp") != std::string::npos)
-				{
+				} else if (mensagem.find("exp") != std::string::npos) {
 
 					cout << "DEBUG Exponencial -> " + mensagem << endl;
 					size_t token = mensagem.find_first_of(",", 0);
@@ -260,9 +222,7 @@ public:
 					message = to_string(exp(stoi(valorString)));
 					resposta.conteudo = message;
 					resposta.pronto = true;
-				}
-				else if (mensagem.find("fat") != std::string::npos)
-				{
+				} else if (mensagem.find("fat") != std::string::npos) {
 
 					cout << "DEBUG Fatorial -> " + mensagem << endl;
 					size_t token = mensagem.find_first_of(",", 0);
@@ -271,9 +231,7 @@ public:
 					message = to_string(fatorial(stoi(valorString)));
 					resposta.conteudo = message;
 					resposta.pronto = true;
-				}
-				else if (mensagem.find("primo") != std::string::npos)
-				{
+				} else if (mensagem.find("primo") != std::string::npos) {
 
 					cout << "DEBUG É primo -> " + mensagem << endl;
 					size_t token = mensagem.find_first_of(",", 0);
@@ -282,16 +240,11 @@ public:
 					message = isPrimo(stoi(valorString));
 					resposta.conteudo = message;
 					resposta.pronto = true;
-				}
-				else
-				{
+				} else {
 
 					cout << "DEBUG Não entendi -> " + mensagem << endl;
-					size_t token = mensagem.find_first_of(",", 0);
-					string valorString = mensagem.substr(token + 1, mensagem.size());
-
-					message = valorString;
-					resposta.conteudo = message;
+					
+					resposta.conteudo = "Não entendi -> " + mensagem;
 					resposta.pronto = true;
 				}
 				/*} else { //açoes dos outros servidores
@@ -365,31 +318,23 @@ public:
 					}
 				}*/
 			}
-			else
-			{
-				cout << "Essa mensagem nem devia ter chegado aqui e será descartada!" << endl;
-			}
+		} else {
+			cout << "Essa mensagem nem devia ter chegado aqui e será descartada!" << endl;
 		}
 	}
 
-	static long fibonacci(int n)
-	{
+	static long fibonacci(int n) {
 		int cont;
 		long int x = 0, z = 1;
 
-		if (n % 2 == 0)
-		{
-			for (cont = 2; cont != n; cont = cont + 2)
-			{
+		if (n % 2 == 0) {
+			for (cont = 2; cont != n; cont = cont + 2) {
 				x = x + z;
 				z = x + z;
 			}
 			return z;
-		}
-		else
-		{
-			for (cont = 1; cont != n; cont = cont + 2)
-			{
+		} else {
+			for (cont = 1; cont != n; cont = cont + 2) {
 				x = x + z;
 				z = x + z;
 			}
@@ -397,27 +342,28 @@ public:
 		}
 	}
 
-	static long fatorial(int n)
-	{
+	static long fatorial(int n) {
 		int aux = 1, prod = 1;
 
-		while (aux <= n)
-		{
+		while (aux <= n) {
 			prod *= aux;
 			aux++;
 		}
 		return prod;
 	}
 
-	static string isPrimo(int num)
-	{
-		int aux, div = 0;
+	static string isPrimo(int num) {
+		int aux;
+		bool primo = true;
 
-		for (aux = 2; aux <= sqrt(num); aux++)
-			if (num % aux == 0)
-				div++;
+		for (aux = 2; aux <= sqrt(num); aux++) {
+			if (num % aux == 0) {
+				primo = false;
+				break;
+			}
+		}
 
-		if (div == 2)
+		if (primo)
 			return "É primo";
 		else
 			return "Não é primo";
